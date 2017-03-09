c590b8c1f46972753fac427368581901cb44b09e
iagon <iangonnella@gmail.com>
2017-03-09 09:29:18 -0800

    Add final comments and setup for vampire program

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,20 +1,21 @@
 
-# Ask for how many employees.
+
 puts "How many employees will be processed:"
   processed = gets.chomp.to_i
 # Generate iterator for while loop
-  i=0 
-  while i < processed
+   i=0 
+   while i < processed
 
-# Ask for name
 puts "What is your name?"
 name = gets.chomp.downcase
 
-# Ask age and year they were born.
-puts "How old are you? What year were you born?: (age,year)"
+puts "How old are you? What year were you born? (age,year)"
 age = gets.chomp
-# Used google to help get this result. Split the string array at ",". 
-# Then .map which runs the block for each element in array and outputting returned value from block.
+=begin Used google to help get this result. First Split the string array at "," to make an array. 
+Originally I tried .each method, but it didn't have intended effect. From what I gathered 
+map/collect will make a new array (map! will change original), while each just runs every 
+element array in through |var|. 
+=end 
  age = age.split(',').map {|x| x.to_i}
  age = age[0] + age[1]
    if age == 2016
@@ -25,27 +26,26 @@ age = gets.chomp
       age = FALSE
    end
 
-# Ask if they want garlic bread
 puts "Our company cafeteria serves garlic bread. Should we order some for you? (Yes/No)"
 g_bread = gets.chomp.downcase
-# If they don't answer yes no repeat
+  
   while g_bread != "yes" && g_bread != "no"
   puts "Our company cafeteria serves garlic bread. Should we order some for you? (Yes/No)"
   g_bread = gets.chomp.downcase
   end
-# Ditto for above but for health insurance
+
 puts "Would you like to enroll in the company's health insurance? (Yes/No)"
 health = gets.chomp.downcase
+  
   while health != "yes" && health != "no"
   puts "Our company cafeteria serves garlic bread. Should we order some for you? (Yes/No)"
   garlic_bread = gets.chomp.downcase
   end
   
-# Had hard time figuring out how to put "Results inconclusive". Initially tried to set it if all parameters failed. 
-# Setting it as default was much more elegant.
+# Had hard time figuring out how to get "Results inconclusive". Initially tried to set it if all conditions failed. 
+# Setting it as default was much simpler.
   is_vamp = "Results Inconclusive"
 
-# Conditional Statements
 if age && (g_bread == "yes" || health == "yes")
   is_vamp = "Probably not a Vampire."
 end 
@@ -62,7 +62,7 @@ if  name == "drake cula" || name == "tu fang"
   is_vamp = "Definitely a vampire"
 end
  
-# Allergies loop
+
 allergies  = ""
  
 until allergies == "done" do
@@ -78,8 +78,8 @@ end
 puts is_vamp
 
 # Increase iterator to avoid continuous loop
-i += 1
-end
+  i += 1
+  end
 
 puts "Actually, never mind! What do these questions have to do with anything? Let's all be friends."
 
