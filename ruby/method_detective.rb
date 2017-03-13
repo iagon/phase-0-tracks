d7158aa117d70f4a3d3f53b877b55997f07818d9
iagon <iangonnella@gmail.com>
2017-03-13 12:23:33 -0700

    Add more methods for string cases

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -8,47 +8,42 @@
 # => “InVeStIgAtIoN”
 
 
-
 "zom".gsub(/[o]/, 'oo')
 "zom".replace "zoom"
 "zom".insert(1, 'o')
 # => “zoom”
 
- "enhance".center(15)
+"enhance".center(15)
 # => "    enhance    "
 
 
-
-
-
- "Stop! You’re under arrest!".upcase
+"Stop! You’re under arrest!".upcase
 # => "STOP! YOU’RE UNDER ARREST!"
 
 
-
-
- "the usual".+" suspects"
+"the usual".+" suspects"
+"the usual".insert(9, " suspects")
 #=> "the usual suspects"
 
 
-
-
-
-
-
-# " suspects".<???>
+" suspects".insert(0, "the usual")
+" suspects".prepend("the usual")
 # => "the usual suspects"
 
-# "The case of the disappearing last letter".<???>
+"The case of the disappearing last letter".slice(0...-1)
+"The case of the disappearing last letter"[0..-2]
 # => "The case of the disappearing last lette"
 
-# "The mystery of the missing first letter".<???>
+"The mystery of the missing first letter".slice(1..-1)
+"The mystery of the missing first letter"[1..-1]
 # => "he mystery of the missing first letter"
 
-# "Elementary,    my   dear        Watson!".<???>
+"Elementary,    my   dear        Watson!".squeeze
 # => "Elementary, my dear Watson!"
 
-# "z".<???>
+"z".getbyte(0)
+"z".each_byte {|x| p x}
+"z".ord
 # => 122 
 # (What is the significance of the number 122 in relation to the character z?)
 
