50f933090d4b20340ba9468929ea2dfaddbd24cb
iagon <iangonnella@gmail.com>
2017-03-17 11:01:44 -0700

    Add methods that change vowels and consonants

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,7 +1,22 @@
 def flip_string(str)
   arr = str.split
-  arr.reverse
+  arr.reverse.join(" ")
+end
+
+def vowel_change(str)
+  flip_string(str).tr!("aeiouAEIOU", "eiouaEIOUA")
+end
+
+def consonant_change(str)
+  vowel_change(str).tr!('bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ', 'cdfghjklmnpqrstvwxyzbCDFGHJKLMNPQRSTVWXYZB')
 end
 
 
-p flip_string("hello world")
+
+
+puts "Enter your spy name(type 'quit' to exit):"
+name = gets.chomp
+
+p "Your #{name} is actually #{consonant_change(name)}"
+
+
