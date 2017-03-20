398079a85642ac52b2312eb18519687b99946a5d
iagon <iangonnella@gmail.com>
2017-03-20 11:42:32 -0700

    Add own class with instance methods

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,3 +1,4 @@
+=begin
 class Puppy
 
   def fetch(toy)
@@ -23,19 +24,48 @@ class Puppy
     p "Extends paw to shake"
   end
 
-
+  def initialize
+    p "Initializing new puppy instance..."
+  end
 
 
 
 end
-
-
 
 Puppy.new.fetch("rubber")
 Puppy.new.speak(2)
 Puppy.new.roll_over
 Puppy.new.dog_years(5)
 Puppy.new.shake
+=end
+
+class Aardvark
+  def initialize
+    p "This is an Aardvark"
+  end
+
+  def eats_ants(int)
+    int.times do
+      puts "Aardvark ate #{int} ants."
+  end
+  end
+
+  def sleeps
+    p "Aardvark is asleep."
+  end
 
 
+end
 
+aardvarks = []
+
+50.times do
+  aardvarks << Aardvark.new
+end
+
+aardvarks.each do |aardvark|
+  aardvark.eats_ants(4)
+  aardvark.sleeps
+end
+
+p aardvarks
