196aad71aadc75225ae816a28c9252e32d8b1eee
iagon <iangonnella@gmail.com>
2017-03-21 13:44:04 -0700

    Add mixin module for shout

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,4 +1,32 @@
 module Shout
+  def yell_angrily(words)
+    words + "!!!" + " :("
+  end
+
+  def yelling_happily(words)
+    words + "!!!!" + " :)"
+  end
+end
+
+
+class Gordon_Ramsay
+  include Shout
+end
+
+class Auctioneer
+  include Shout
+end
+
+gr = Gordon_Ramsay.new
+puts gr.yell_angrily("It's Raw")
+puts gr.yelling_happily("Perfect")
+
+auctioneer = Auctioneer.new
+puts auctioneer.yell_angrily("Don't touch the items")
+puts auctioneer.yelling_happily("Going once..Going twice..Sooollld")
+
+=begin
+module Shout
   def self.yell_angrily(words)
     words + "!!!" + " :("
   end
@@ -11,3 +39,4 @@ end
 
 Shout.yell_angrily("noooo")
 Shout.yelling_happily("yesss")
+=end
