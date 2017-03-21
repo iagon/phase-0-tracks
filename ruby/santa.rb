700c174d1cd678ba23fc69c23cab6851d1d09900
iagon <iangonnella@gmail.com>
2017-03-20 18:26:38 -0700

    Add attr reader/writer methods and program to create santas

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,4 +1,7 @@
 class Santa
+  attr_reader :ethnicity
+  attr_accessor :gender, :age
+
 
 def speak 
   puts "Ho, ho, ho! Haaaappy holidays!"
@@ -14,11 +17,25 @@ def initialize(gender, ethnicity)
   @ethnicity = ethnicity
   @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
   @age = 0
-
-end
 end
 
-=begin
+def celebrate_birthday 
+  @age = @age + 1
+end
+
+def get_mad_at(reindeer_name)
+  @reindeer_ranking.remove(reindeer_name)
+  @reindeer_ranking << reindeer_name
+end
+
+end
+
+=begin 
+DRIVER CODE
+santa = Santa.new
+santa.speak
+santa.eat_milk_and_cookies("Peanut Butter")
+
 santas = []
 example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
 example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
@@ -29,8 +46,20 @@ end
 
 p santas
 
-
-santa = Santa.new
-santa.speak
-santa.eat_milk_and_cookies("Peanut Butter")
+puts santas[2].age
+puts santas[2].celebrate_birthday
+puts santas[2].age
+puts santas[2].ethnicity
+puts santas[2].gender = "Crazy"
 =end
+
+santas = []
+example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+
+150.times do |i|
+  santa_create = Santa.new(example_genders.sample, example_ethnicities.sample)
+  santa_create.age = rand(0..140)
+  puts "Santa number #{i}: is #{santa_create.age} years old and identifies as a #{santa_create.gender}, #{santa_create.ethnicity}."
+
+end
