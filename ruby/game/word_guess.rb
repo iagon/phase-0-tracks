7cc418e99a7b21d75c4c66379f53d8b65a97c211
iagon <iangonnella@gmail.com>
2017-03-24 09:53:20 -0700

    Update with win lose scenarios

--- ruby/game/word_guess.rb
+++ ruby/game/word_guess.rb
@@ -5,17 +5,38 @@ class WordGame
   def initialize(master_word)
     @master_word = master_word
     @master_arr = master_word.split('')
-    @guess_count = 0
     @letter_guess = ('_' * master_word.length).split('')
+    @past_guess = []
+    @total_guesses = master_word.length + 3
   end
 
   def guess(letter)
+    if @past_guess.include?(letter)
+        puts "That letter has already been guessed."
+    end
     if @master_arr.include?(letter)
        @letter_guess.delete_at(@master_arr.index(letter))
        @letter_guess.insert(@master_arr.index(letter), letter)
-       p @letter_guess.join(" ")
+       @letter_guess.join(" ")
+       p "Letter match! Updated board:"
+       p @letter_guess.join('')
+    else 
+      puts "Nope, letter not included."
     end
+    @past_guess << letter
+    @total_guesses -= 1
   end
+  win if @letter_guess.join("") == @master_word
+  lose if @total_guesses == 0
+
+  def win
+    puts "Wow you got it!"
+  end
+
+  def lose
+    puts "Nope better luck next time sucker."
+  end
+
 
 
 end
