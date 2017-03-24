15580ef9ef1f5e4ef833f5a3bed4176fbc7379c1
iagon <iangonnella@gmail.com>
2017-03-23 18:12:17 -0700

    Add case for guessed letter to show in array

--- ruby/game/word_guess.rb
+++ ruby/game/word_guess.rb
@@ -1,14 +1,23 @@
 class WordGame
-  attr_reader :@guess_count, :@is_over
-
-  def intialize(str)
-    @master_word =  input
-    @guess_count = 0
-    @word_guess = ('_' * input.length).split('')
-    @is_over = false
-  end
-
-  def 
+  attr_reader :guess_count, :letter_guess
   
 
+  def initialize(master_word)
+    @master_word = master_word
+    @master_arr = master_word.split('')
+    @guess_count = 0
+    @letter_guess = ('_' * master_word.length).split('')
+  end
+
+  def guess(letter)
+    if @master_arr.include?(letter)
+       @letter_guess.delete_at(@master_arr.index(letter))
+       @letter_guess.insert(@master_arr.index(letter), letter)
+       p @letter_guess.join(" ")
+    end
+  end
+
+
 end
+
+
