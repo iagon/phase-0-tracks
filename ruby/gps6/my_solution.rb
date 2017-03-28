62ec948dc7aca8b58eb1e3662cf035ea7e85cfdc
iagon <iangonnella@gmail.com>
2017-03-27 17:17:54 -0700

    Add work done during gps

--- ruby/gps6/my_solution.rb
+++ ruby/gps6/my_solution.rb
@@ -4,62 +4,68 @@
 # We spent [#] hours on this challenge.
 
 # EXPLANATION OF require_relative
-#
-#
+# require relative can take the path from your current directory, whereas require can only handle paths relative to the base directory
+# it is a shortcut subset of require 
+
 require_relative 'state_data'
 
 class VirusPredictor
 
+#initializes a new instance of the virus predictor with the attributes entered as parameters
   def initialize(state_of_origin, population_density, population)
     @state = state_of_origin
     @population = population
     @population_density = population_density
   end
 
+
+
+#calls 2 methods 
   def virus_effects
-    predicted_deaths(@population_density, @population, @state)
-    speed_of_spread(@population_density, @state)
+    print "#{@state} will lose #{number_of_predicted_deaths} people in this outbreak"
+    puts " and will spread across the state in #{speed_of_spread} months.\n\n"
   end
 
   private
 
-  def predicted_deaths(population_density, population, state)
-    # predicted deaths is solely based on population density
+#calculates numbers of deaths based on population density and pop size and prints a string w the results / returns nil
+def death_factor  
+
     if @population_density >= 200
-      number_of_deaths = (@population * 0.4).floor
+      0.4
     elsif @population_density >= 150
-      number_of_deaths = (@population * 0.3).floor
+      0.3
     elsif @population_density >= 100
-      number_of_deaths = (@population * 0.2).floor
+      0.2
     elsif @population_density >= 50
-      number_of_deaths = (@population * 0.1).floor
+      0.1
     else
-      number_of_deaths = (@population * 0.05).floor
+      0.05
     end
-
-    print "#{@state} will lose #{number_of_deaths} people in this outbreak"
-
   end
 
-  def speed_of_spread(population_density, state) #in months
+  def number_of_predicted_deaths
+    # predicted deaths is solely based on population density
+
+    (@population * death_factor).floor
+  end
+
+#calculates speed of spread based on population density and prints string / returns nil
+  def speed_of_spread #in months
     # We are still perfecting our formula here. The speed is also affected
     # by additional factors we haven't added into this functionality.
-    speed = 0.0
 
     if @population_density >= 200
-      speed += 0.5
+      0.5
     elsif @population_density >= 150
-      speed += 1
+      1
     elsif @population_density >= 100
-      speed += 1.5
+      1.5
     elsif @population_density >= 50
-      speed += 2
+      2
     else
-      speed += 2.5
+      2.5
     end
-
-    puts " and will spread across the state in #{speed} months.\n\n"
-
   end
 
 end
@@ -81,6 +87,11 @@ california.virus_effects
 
 alaska = VirusPredictor.new("Alaska", STATE_DATA["Alaska"][:population_density], STATE_DATA["Alaska"][:population])
 alaska.virus_effects
+
+STATE_DATA.each do |state, info_hash|
+state = VirusPredictor.new(state, info_hash[:population_density], info_hash[:population])
+state.virus_effects
+end
 
 
 #=======================================================================
