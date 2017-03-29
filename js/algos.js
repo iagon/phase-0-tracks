f9498e6ab539fe710bf228ff51c3885456a91c0a
iagon <iangonnella@gmail.com>
2017-03-29 13:07:31 -0700

    Clarify pseudocode format driver code

--- js/algos.js
+++ js/algos.js
@@ -1,7 +1,9 @@
 // Write a function to return longest phrase or word in an array.
-// Index through array, comparing the length of first object to a var set to zero
-// Replace var with first object repeat comparison for all objects of array
-// After all indexes compared return string storing longest array index
+// Iterate through array for total length of array
+// Compare length of first array index to var set to zero
+// Replace var with first array index, compare second array index to first which is now set at var
+// After all indexes compared, store var containing longest string in array indexes to string
+// Return string
 
 function longestString(arr) {
   var currentLong = 0;
@@ -15,10 +17,16 @@ function longestString(arr) {
   return longestAll;
 }
 
-var arrayOne = ["Long", "Longest", "Longer", "Longer than the longest?"]
-var arrayTwo = ["Some", "Fun", "for all the", "cold ones"]
-var arrayThree = [".", "..", ".......", "....","............."]
+// 
 
-console.log(longestString(arrayOne));
-console.log(longestString(arrayTwo));
-console.log(longestString(arrayThree));
+
+
+
+
+
+
+
+
+console.log(longestString(["Long", "Longest", "Longer", "Longer than the longest?"]));
+console.log(longestString(["Some", "Fun", "for all the", "cold ones"]));
+console.log(longestString([".", "..", ".......", "....","............."]));
