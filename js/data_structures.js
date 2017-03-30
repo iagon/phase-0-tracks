0905ecf16a282065d8ca07973acd85f8b9456c5d
iagon <iangonnella@gmail.com>
2017-03-30 09:48:58 -0700

    Add object for key values

--- js/data_structures.js
+++ js/data_structures.js
@@ -3,3 +3,9 @@ var names = ["Tom", "Greg", "Jerry", "Horse"]
 
 colors.push("mauve");
 names.push("Donald");
+
+var horses = {}
+for (var i =0; i < names.length; i++) {
+  horses[names[i]] = colors[i];
+}
+
