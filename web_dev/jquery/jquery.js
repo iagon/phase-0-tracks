12005bfcc9f0bb254760523810c5fc59a19c7e02
iagon <iangonnella@gmail.com>
2017-04-13 14:44:54 -0700

    Add final jquery functions

--- web_dev/jquery/jquery.js
+++ web_dev/jquery/jquery.js
@@ -1,4 +1,36 @@
-$(document).ready(function() {
-  $("h1").css({"color": "green"})
+$(document).ready(function(){
+    $("div").hide();
 
-};
+    $(".button1").click(function(){
+        $("div").show();
+    });
+
+    $(".button2").click(function(){
+        $("ol").fadeOut()
+    });
+
+    $(".button3").click(function(){
+        $("ol").fadeIn();
+    });
+
+    $("h1").mouseenter(function(){
+        $("h1").css("background-color", "red");
+    });
+
+    $("h1").mouseleave(function(){
+        $("h1").css("background-color", "black");
+    });
+
+
+});
+
+
+/*
+    $("article").hover(function(){
+        alert("You entered the article");
+    });,
+
+    function(){
+      alert("You left the article");
+    });
+*/
--- web_dev/jquery/notes.md
+++ web_dev/jquery/notes.md
@@ -1,3 +1,8 @@
 #### Plan
 
 I hope to open find a docs for jquery and briefly glance over some topics. After a broad overview of jquery I will search for a jquery tutorials. I hope to find a problem based tutorial to assist in testing while learning. At only two hours no real break is needed, but perhaps around the 75 minute mark I will do something for a minute or two to give the eyes a rest from the computer screen. 
+
+
+#### Results
+
+I found W3schools tutorial which I remember from html/css days. They offer a nice way to test concepts out with examples. The docs although very extensive weren't all that helpful for a beginner. 
